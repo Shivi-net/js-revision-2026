a03b10478bbee5406e632607b096472bf588a5a9
Shivalika Sharma <shivalikakhajuria@gmail.com>
2026-03-11 17:37:19 +0530

    use of this in arrow and regular funtion

--- day-5-this-in-eventListener/index.js
+++ day-5-this-in-eventListener/index.js
@@ -3,3 +3,7 @@ const button = document.getElementById('btn')
 button.addEventListener("click", function(){
     console.log(this)
 })
+
+// button.addEventListener("click", ()=>{
+//     console.log(this)
+// })
